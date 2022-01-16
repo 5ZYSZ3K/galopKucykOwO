ffcb20d10b22f31d0267cfd9b53c19b8a2eaea4b
Dudi4444 <72911290+Dudi4444@users.noreply.github.com>
2022-01-16 11:34:06 +0100

    Ostatnia wersja

--- src/components/Loogged/Tips.js
+++ src/components/Loogged/Tips.js
@@ -5,13 +5,21 @@ import "../../styles/tips.css";
 
 export default function Tips() {
   const [tip, setTip] = useState("");
+  const [tip2, setTip2] = useState("");
   useEffect(() => {
     axios.get("http://localhost:5000/tips/").then((data) => {
       setTip(data.data.tip);
     });
+    axios.get("http://localhost:5000/links/").then((data) => {
+      console.log(data.data)
+      setTip2(data.data.link);
+    });
   }, []);
-  return (<div className="tak">
+  return (<div><div className="tak">
     <div className="top">Porada:</div>
     <div className="down">{tip}</div>
-    </div>)
+    </div><div className="tak">
+    <div className="top">Zobacz też:</div>
+    <div className="down">{tip2}</div>
+    </div></div>)
 }
